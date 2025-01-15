914ef2fd4491f04d70a81a65aff7d7793ed402ab
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-01-14 21:24:54 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("watchButton").addEventListener("click", function () {
+  const trailerUrl = "https://www.youtube.com/watch?v=mbbPSq63yuM&t=4s";
+  window.open(trailerUrl, "_blank", "fullscreen=yes");
+});
