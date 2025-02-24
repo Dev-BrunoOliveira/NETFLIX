9bc70dc18218916b79a85a53ac7013a9133bfd6e
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-02-24 18:16:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.getElementById('watchButton').addEventListener('click', function() {
+    var videoContainer = document.querySelector('#video-container');
+    var moviePlayer = document.getElementById('moviePlayer');
+  
+    videoContainer.style.display = 'block';
+  
+    if (moviePlayer.requestFullscreen) {
+        moviePlayer.requestFullscreen();
+    } else if (moviePlayer.mozRequestFullScreen) { 
+        moviePlayer.mozRequestFullScreen();
+    } else if (moviePlayer.webkitRequestFullscreen) { 
+        moviePlayer.webkitRequestFullscreen();
+    } else if (moviePlayer.msRequestFullscreen) { 
+        moviePlayer.msRequestFullscreen();
+    }
+  
+    moviePlayer.play();
+  });
+  
+  function exitFullscreen() {
+    var moviePlayer = document.getElementById('moviePlayer');
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { 
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) { 
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) { 
+      document.msExitFullscreen();
+    }
+  }
+  
+  document.getElementById('closeButton').addEventListener('click', function() {
+   
+    exitFullscreen();
+  
+    var videoContainer = document.querySelector('#video-container');
+    videoContainer.style.display = 'none';
+  });
+  
