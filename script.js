c083b5bbb1431e2e3469c3204e378d2deae5bef8
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-03-01 23:24:32 -0300

    correção do autoplay

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-document.getElementById('watchButton').addEventListener('click', function() {
-    var videoContainer = document.querySelector('#video-container');
-    var moviePlayer = document.getElementById('moviePlayer');
-  
-    videoContainer.style.display = 'block';
-  
-    if (moviePlayer.requestFullscreen) {
-        moviePlayer.requestFullscreen();
-    } else if (moviePlayer.mozRequestFullScreen) { 
-        moviePlayer.mozRequestFullScreen();
-    } else if (moviePlayer.webkitRequestFullscreen) { 
-        moviePlayer.webkitRequestFullscreen();
-    } else if (moviePlayer.msRequestFullscreen) { 
-        moviePlayer.msRequestFullscreen();
-    }
-  
-    moviePlayer.play();
-  });
-  
-  function exitFullscreen() {
-    var moviePlayer = document.getElementById('moviePlayer');
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { 
-      document.webkitExitFullscreen();
-    } else if (document.mozCancelFullScreen) { 
-      document.mozCancelFullScreen();
-    } else if (document.msExitFullscreen) { 
-      document.msExitFullscreen();
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  var moviePlayer = document.getElementById("moviePlayer");
+  moviePlayer.pause();
+});
+
+document.getElementById("watchButton").addEventListener("click", function () {
+  var videoContainer = document.querySelector("#video-container");
+  var moviePlayer = document.getElementById("moviePlayer");
+
+  videoContainer.style.display = "block";
+
+  if (moviePlayer.requestFullscreen) {
+    moviePlayer.requestFullscreen();
+  } else if (moviePlayer.mozRequestFullScreen) {
+    moviePlayer.mozRequestFullScreen();
+  } else if (moviePlayer.webkitRequestFullscreen) {
+    moviePlayer.webkitRequestFullscreen();
+  } else if (moviePlayer.msRequestFullscreen) {
+    moviePlayer.msRequestFullscreen();
   }
-  
-  document.getElementById('closeButton').addEventListener('click', function() {
-   
-    exitFullscreen();
-  
-    var videoContainer = document.querySelector('#video-container');
-    videoContainer.style.display = 'none';
-  });
-  
+
+  moviePlayer.play();
+});
+
+function exitFullscreen() {
+  var moviePlayer = document.getElementById("moviePlayer");
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+document.getElementById("closeButton").addEventListener("click", function () {
+  exitFullscreen();
+
+  var videoContainer = document.querySelector("#video-container");
+  videoContainer.style.display = "none";
+});
